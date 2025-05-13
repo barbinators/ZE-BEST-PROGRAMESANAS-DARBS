20a2d1d14e0b2ec8d18c86bca7f4a47c8096d448
barbinators <marisdemida8@gmail.com>
2025-05-13 19:27:58 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+function filtrēt(kategorija) {
+    let bildes = document.querySelectorAll(".attēls");
+
+    for (let i = 0; i < bildes.length; i++) {
+        if (kategorija === "visi"  bildes[i].classList.contains(kategorija)) {
+            bildes[i].style.display = "inline-block";
+        } else {
+            bildes[i].style.display = "none";
+        }
+    }
+}
+
+function pārbaude() {
+    let vārds = document.getElementById("vārds").value;
+    let ziņa = document.getElementById("ziņa").value;
+
+    if (vārds === ""  ziņa === "") {
+        alert("Lūdzu, aizpildi visus laukus!");
+        return false;
+    }
+
+    alert("Paldies par ziņu!");
+    return true;
+}
